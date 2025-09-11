a4507957b294d914a7f5ef0ae53122a0cccc6960
RicTBest <rictbest@gmail.com>
2025-09-11 15:22:16 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -558,7 +558,6 @@ export default function Home() {
                       <div>
                         <h2 className="text-2xl sm:text-3xl font-black text-gray-800 flex items-center space-x-2 sm:space-x-3 mb-1">
                           <span>{owner.name}</span>
-                          <span>{owner.num_gooses}</span>
                           {gooseEggs && <span className={`text-3xl sm:text-4xl ${numGooses > 0 ? 'animate-bounce' : ''}`}>{gooseEggs}</span>}
                           {getRankEmoji(rank) && <span className={`text-3xl sm:text-4xl ${rank === 1 ? 'animate-bounce' : ''}`}>{getRankEmoji(rank)}</span>}
                         </h2>
