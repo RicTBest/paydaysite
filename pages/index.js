1d36121bf64c39c74ac85a98be19e03209534ffb
RicTBest <rictbest@gmail.com>
2025-11-16 17:36:13 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -721,7 +721,7 @@ useEffect(() => {
                       <div>
                         <h2 className="text-2xl sm:text-3xl font-black text-gray-800 flex items-center space-x-2 sm:space-x-3 mb-1">
                           <span>{owner.name}</span>
-                          <span>getTrophyEmoji(name)</span>
+                          <span>{getTrophyEmoji(name)}</span>
                           {gooseEggs && <span className={`text-3xl sm:text-4xl ${numGooses > 0 ? 'animate-bounce' : ''}`}>{gooseEggs}</span>}
                           {getRankEmoji(rank) && <span className={`text-3xl sm:text-4xl ${rank === 1 ? 'animate-bounce' : ''}`}>{getRankEmoji(rank)}</span>}
                         </h2>
